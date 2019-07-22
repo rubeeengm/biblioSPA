c6a4fd6eec0d2c0c9e529e4a96e4454e527d729b
Ruben Garcia <rubeeengm@gmail.com>
2019-07-22 09:06:44 -0500

    navegación SPA incluída

--- src/main/webapp/vistas/login/Login.js
+++ src/main/webapp/vistas/login/Login.js
@@ -13,23 +13,29 @@ var Login = {
         console.log("estoy logueado");
     },
     registrarAlumno: function () {
-        $usuario = $('#usuario').val();
-        $contrasenia = $('#contrasenia').val();
+//        $usuario = $('#usuario').val();
+//        $contrasenia = $('#contrasenia').val();
         
-        $.ajax({
-            url: "http://localhost:8084/biblioSPA/Controlador",
-            method: "POST",
-            data: JSON.stringify({
-                ACCION: 'login',
-                usuario: $usuario, 
-                contrasenia: $contrasenia
-            })
-//            data: {
+//        $.ajax({
+//            url: "http://localhost:8080/biblioSPA/Controlador",
+//            method: "POST",
+//            data: JSON.stringify({
 //                ACCION: 'login',
-//                usuario: $usuario,
+//                usuario: $usuario, 
 //                contrasenia: $contrasenia
-//            }
-        }).done(function () {
+//            })
+////            data: {
+////                ACCION: 'login',
+////                usuario: $usuario,
+////                contrasenia: $contrasenia
+////            }
+//        }).done(function () {
+//            $('div.container').html(arguments[0]);
+//        });
+
+        $.ajax({
+            url:"http://localhost:8080/biblioSPA/vistas/formularios/RegistroAlumno.jsp"
+        }).done(function(){
             $('div.container').html(arguments[0]);
         });
     }
