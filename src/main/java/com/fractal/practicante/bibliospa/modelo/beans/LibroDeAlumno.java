1b7dd6b309adf70527da06297ca3a5d8fd2701ed
Jorge Hernández <mperez@fractalinside.com>
2019-07-22 14:03:13 -0500

    Comentarios y formateo de la clase LibroDeAlumno.java

--- src/main/java/com/fractal/practicante/bibliospa/modelo/beans/LibroDeAlumno.java
+++ src/main/java/com/fractal/practicante/bibliospa/modelo/beans/LibroDeAlumno.java
@@ -1,34 +1,74 @@
 package com.fractal.practicante.bibliospa.modelo.beans;
 
+/**
+ * Clase LibroDeAlumno.java, representa la vinculación que hay entre el alumno 
+ * y sus libros.
+ * @author Jorge Hernandez
+ */
 public class LibroDeAlumno {
 
+    /** 
+     * Atributo id: entero que representa a la vinculacion del libro y el 
+     * alumno 
+     */
     private int id;
+    
+    /** Atributo idUsuario: entero que representa al usuario dueño del libro */
     private int idUsuario;
+    
+    /** Atributo idLibro: entero que representa al libro del dueño */
     private int idLibro;
-
-    public int getId() {
-        return id;
+    
+    /**
+     * Constructor básico de LibroDeAlumno
+     */
+    public LibroDeAlumno() {
     }
-
-    public int getIdUsuario() {
-        return idUsuario;
-    }
-
-    public int getIdLibro() {
-        return idLibro;
-    }
-
+    
+    /**
+     * Constructor de LibroDeAlumno que usa los atributos idUsuario e idLibro
+     * @param idUsuario     ID del usuario
+     * @param idLibro       ID del libro
+     */
     public LibroDeAlumno(int idUsuario, int idLibro) {
         this.idUsuario = idUsuario;
         this.idLibro = idLibro;
     }
     
+    /**
+     * Constructor de LibroDeAlumno que usa los atributos id, idUsuario 
+     * e idLibro
+     * @param id            ID de la vinculación
+     * @param idUsuario     ID del usuario
+     * @param idLibro       ID del libro
+     */
     public LibroDeAlumno(int id, int idUsuario, int idLibro) {
         this.id = id;
         this.idUsuario = idUsuario;
         this.idLibro = idLibro;
+    }    
+
+    /**
+     * Metodo obtener para el id
+     * @return ID de la vinculación
+     */
+    public int getId() {
+        return id;
     }
     
-    public LibroDeAlumno() {
+    /**
+     * Metodo obtener del ID del usuario
+     * @return ID del usuario
+     */
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     * Metodo obtener del ID del libro
+     * @return ID del libro
+     */
+    public int getIdLibro() {
+        return idLibro;
     }
 }
