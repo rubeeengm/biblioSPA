39f67a66d7bfd515527763353c0268d5d1a89bcf
Jorge Hernández <jaherdezn@hotmail.com>
2019-07-25 13:11:37 -0500

    Los archivos en la carpeta modelos fueron reformateados y depurados, se eliminó codigo basura y se agregaron los formatos de acuerdo a los lineamientos.

--- src/main/java/com/fractal/practicante/bibliospa/modelo/modelos/ModeloAlumnos.java
+++ src/main/java/com/fractal/practicante/bibliospa/modelo/modelos/ModeloAlumnos.java
@@ -9,53 +9,32 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ModeloAlumnos implements OperacionObtener<Alumno>, OperacionObtenerTodos<Alumno> {
-
-    /**
-     * Método que al recibir una conexión y un objeto Alumno, manda a llamar al
-     * procedimiento adecuado de la BD para insertar un nuevo Alumno en la tabla
-     * de la BD.
-     *
-     * @param conexion Conexión a la base de datos que se utilizará.
-     * @param alumno Objeto Alumno con todos sus atributos con información para
-     * poder insertar uno nuevo en la BD con ella.
-     * @throws SQLException
-     */
-    /*
-    public boolean insertar(Connection conexion, Alumno alumno) throws SQLException{
-        try{
-            PreparedStatement consulta;
-            consulta = conexion.prepareStatement("CALL USP_ALUMNOS_C (?, ?, ?, ?, ?, ?);");
-            consulta.setString(1, alumno.getNombre());
-            consulta.setString(2, alumno.getpApellido());
-            consulta.setString(3, alumno.getsApellido());
-            consulta.setString(4, alumno.getTelefono());
-            consulta.setString(5, alumno.getDni());
-            consulta.setInt(6, alumno.getIdUsuario());
-            consulta.executeUpdate();
-            return true;
-        }
-        catch(SQLException error){
-            return false;
-        }
-    }*/
+/**
+ * Clase que implenta las clases OperacionObtener y OperacionObtenerTodos, los 
+ * metodos devuelven un objeto Alumno extraido de la BD.
+ * @author Marco Lopez
+ */
+public class ModeloAlumnos implements OperacionObtener<Alumno>, 
+        OperacionObtenerTodos<Alumno> {
 
     /**
      * Método que al recibir una conexión y un entero, manda a llamar al
      * procedimiento adecuado de la BD para regresar un objeto con todos los
      * atributos de un Alumno en particular.
      *
-     * @param conexion Conexión a la base de datos que se utilizará.
-     * @param id Entero que se usará en la búsqueda de un Alumno por su ID en la
-     * BD.
-     * @return Alumno cuyo ID es el solicitado y con todos sus atributos con su
-     * respectiva información.
+     * @param conexion  Conexión a la base de datos que se utilizará.
+     * @param id        Entero que se usará en la búsqueda de un Alumno por su 
+     *                  ID en la BD.
+     * @return          Alumno cuyo ID es el solicitado y con todos sus 
+     *                  atributos con su respectiva información.
      * @throws SQLException
      */
     @Override
     public Alumno obtener(Connection conexion, int id) throws SQLException {
         Alumno alumno = null;
-        PreparedStatement consulta = conexion.prepareStatement("CALL USP_ALUMNOS_R (?);");
+        PreparedStatement consulta = conexion.prepareStatement(
+                "CALL USP_ALUMNOS_R (?);"
+        );
         consulta.setInt(1, id);
         ResultSet resultado = consulta.executeQuery();
         while (resultado.next()) {
@@ -75,16 +54,19 @@ public class ModeloAlumnos implements OperacionObtener<Alumno>, OperacionObtener
      * adecuado de la BD para regresar un ArrayList con todos los Alumnos
      * registrados en la BD.
      *
-     * @param conexion Conexión a la base de datos que se utilizará.
-     * @return ArrayList de objetos de tipo Alumno igual al número de Alumnos
-     * registrados en la BD con todos sus datos.
+     * @param conexion  Conexión a la base de datos que se utilizará.
+     * @return          ArrayList de objetos de tipo Alumno igual al número de 
+     *                  Alumnos registrados en la BD con todos sus datos.
      * @throws SQLException
      */
     @Override
-    public ArrayList<Alumno> obtenerTodos(Connection conexion) throws SQLException {
+    public ArrayList<Alumno> obtenerTodos(Connection conexion) 
+            throws SQLException {
         ArrayList<Alumno> alumnos = new ArrayList();
 
-        PreparedStatement consulta = conexion.prepareStatement("CALL USP_ALUMNOS_RTODOS ();");
+        PreparedStatement consulta = conexion.prepareStatement(
+                "CALL USP_ALUMNOS_RTODOS ();"
+        );
         ResultSet resultado = consulta.executeQuery();
         while (resultado.next()) {
             Alumno alumno = null;
@@ -99,27 +81,4 @@ public class ModeloAlumnos implements OperacionObtener<Alumno>, OperacionObtener
         }
         return alumnos;
     }
-
-    /**
-     * Método que al recibir una conexión y un entero, manda a llamar al
-     * procedimiento adecuado de la BD para eliminar un registro con todos los
-     * atributos de un Alumno en particular.
-     *
-     * @param conexion Conexión a la base de datos que se utilizará.
-     * @param id Entero que se usará en la búsqueda de un Alumno por su ID en la
-     * BD.
-     * @throws SQLException
-     */
-    /*
-    public boolean eliminar(Connection conexion, int id) throws SQLException{
-        try{
-            PreparedStatement consulta;
-            consulta = conexion.prepareStatement("CALL USP_ALUMNOS_D (?);");
-            consulta.setInt(1, id);
-            consulta.executeUpdate();
-            return true;
-        }catch(SQLException error){
-            return false;
-        }
-    }*/
 }
--- src/main/java/com/fractal/practicante/bibliospa/modelo/modelos/ModeloUsuarios.java
+++ src/main/java/com/fractal/practicante/bibliospa/modelo/modelos/ModeloUsuarios.java
@@ -7,97 +7,68 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Modelo que implementa la interfaz OperacionObtener, nos permite buscar en la 
+ * BD los usuarios que solicitemos.
+ * @author Marco Lopez
+ */
 public class ModeloUsuarios implements OperacionObtener<Usuario> {
-    /**
-     * Método que al recibir una conexión y un objeto Usuario, manda a llamar al
-     * procedimiento adecuado de la BD para insertar un nuevo Usuario en la
-     * tabla de la BD.
-     *
-     * @param conexion Conexión a la base de datos que se utilizará.
-     * @param usuario Objeto Usuario con todos sus atributos con información
-     * para poder insertar uno nuevo en la BD con ella.
-     * @throws SQLException
-     */
-    /*
-    public boolean insertar(Connection conexion, Usuario usuario) throws SQLException {
-        try{
-            PreparedStatement consulta;
-            consulta = conexion.prepareStatement("CALL USP_USUARIOS_C (?, ?);");
-            consulta.setString(1, usuario.getUsuario());
-            consulta.setString(2, usuario.getContrasenia());
-            consulta.executeUpdate();
-            return true;
-        }catch(SQLException error){
-            return false;
-        }
-    }*/
-
+    
     /**
      * Método que al recibir una conexión y un entero, manda a llamar al
      * procedimiento adecuado de la BD para regresar un objeto con todos los
      * atributos de un Usuario en particular.
      *
-     * @param conexion Conexión a la base de datos que se utilizará.
-     * @param id Entero que se usará en la búsqueda de un Usuario por su ID en
-     * la BD.
-     * @return Usuario cuyo ID es el solicitado y con todos sus atributos con su
-     * respectiva información.
+     * @param conexion  Conexión a la base de datos que se utilizará.
+     * @param id        Entero que se usará en la búsqueda de un Usuario por su 
+     *                  ID en la BD.
+     * @return          Usuario cuyo ID es el solicitado y con todos sus 
+     *                  atributos con su respectiva información.
      * @throws SQLException
      */
     @Override
     public Usuario obtener(Connection conexion, int id) throws SQLException {
         Usuario usuario = null;
-        PreparedStatement consulta = conexion.prepareStatement("CALL USP_USUARIOS_R (?);");
+        PreparedStatement consulta = conexion.prepareStatement(
+            "CALL USP_USUARIOS_R (?);"
+        );
         consulta.setInt(1, id);
         ResultSet resultado = consulta.executeQuery();
         while (resultado.next()) {
-            usuario = new Usuario(Integer.parseInt(resultado.getString("ID")),
+            usuario = new Usuario(Integer.parseInt(
+                resultado.getString("ID")),
                 resultado.getString("USUARIO"),
                 resultado.getString("CONTRASENIA"),
-                resultado.getString("ADMIN").charAt(0));
+                resultado.getString("ADMIN").charAt(0)
+            );
         }
         return usuario;
     }
 
     /**
-     * Método que al recibir una conexión y un entero, manda a llamar al
-     * procedimiento adecuado de la BD para eliminar un registro con todos los
-     * atributos de un Usuario en particular.
-     *
-     * @param conexion Conexión a la base de datos que se utilizará.
-     * @param id Entero que se usará en la búsqueda de un Alumno por su ID en la
-     * BD.
-     * @throws SQLException
-     */
-    /*
-    public boolean eliminar(Connection conexion, int id) throws SQLException{
-        try{
-            PreparedStatement consulta;
-            consulta = conexion.prepareStatement("CALL USP_USUARIOS_D (?);");
-            consulta.setInt(1, id);
-            consulta.executeUpdate();
-            return true;
-        }catch(SQLException error){
-            return false;
-        }
-    }*/
-    /**
-     *
+     * Método que al recibir una conexión y objeto Usuario, manda a llamar al
+     * procedimiento adecuado para comprobar la existencia de dicho Usuario, 
+     * al finalizar regresa el objeto de dicho USuario.
      * @param conexion
      * @param usuario
      * @return
      * @throws SQLException
      */
-    public Usuario verificarLogin(Connection conexion, Usuario usuario) throws SQLException {
-        PreparedStatement consulta = conexion.prepareStatement("CALL USP_USUARIOSLOGIN_R (?, ?); ");
+    public Usuario verificarLogin(Connection conexion, Usuario usuario) 
+            throws SQLException {
+        PreparedStatement consulta = conexion.prepareStatement(
+                "CALL USP_USUARIOSLOGIN_R (?, ?); "
+        );
         consulta.setString(1, usuario.getNombreUsuario());
         consulta.setString(2, usuario.getContrasenia());
         ResultSet resultado = consulta.executeQuery();
         while (resultado.next()) {
-            usuario = new Usuario(Integer.parseInt(resultado.getString("ID")),
+            usuario = new Usuario(Integer.parseInt(
+                resultado.getString("ID")),
                 resultado.getString("USUARIO"),
                 resultado.getString("CONTRASENIA"),
-                resultado.getString("ADMIN").charAt(0));
+                resultado.getString("ADMIN").charAt(0)
+            );
         }
         return usuario;
     }
